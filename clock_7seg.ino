7f78721f7540cca21ba82d959eb39078bdcaef04
Andrew Curtin <adcurtin@gmail.com>
2016-12-17 05:44:58 -0500

    final testing code

--- clock_7seg.ino
+++ clock_7seg.ino
@@ -1,7 +1,8 @@
 
 #include "LedControl.h"
-#include <SparkFunDS1307RTC.h>
 #include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 
 
 /*
@@ -20,10 +21,8 @@ int intensity = 15;
 void setup() {
     Serial.begin(9600);
 
-    rtc.begin();
-    rtc.update();
-    rtc.autoTime();
-    rtc.update();
+    setSyncProvider(RTC.get);
+    setSyncInterval(3600); // sync every hour, hopefully before clock gets 1s off
 
     /*
      The MAX72XX is in power-saving mode on startup,
@@ -36,16 +35,8 @@ void setup() {
 }
 
 void loop() {
-    rtc.update();
+    digitalClockDisplay();
 
-    Serial.print("Time: ");
-    Serial.print(rtc.dayStr());
-    Serial.print(" " + String(rtc.month()) + "/");   // Print month
-    Serial.print(String(rtc.date()) + "/");
-    Serial.print(String(rtc.year()) + " ");
-    Serial.print(String(rtc.hour()) + ":");
-    Serial.print(String(rtc.minute()) + ":");
-    Serial.println(String(rtc.second()));
     //setDigit(chip, digit_position, number, dot);
     // lc.setDigit(0,,i,false);
     if (intensity == 0){
@@ -61,8 +52,7 @@ void loop() {
     delay(1000);
     lc.setLed(0, 4, 0, 0); //DP4
     lc.setLed(0, 0, 4, 1); //D0
-    lc.setIntensity(0,intensity--);
-    Serial.println(String(intensity));
+    lc.setIntensity(0,8);
     digitalWrite(13, HIGH);
     delay(1000);
     digitalWrite(13, LOW);
@@ -71,43 +61,24 @@ void loop() {
 
 }
 
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.println();
+}
 
-// /*
-//  This method will display the characters for the
-//  word "Arduino" one after the other on digit 0.
-//  */
-// void writeArduinoOn7Segment() {
-//   lc.setChar(0,0,'a',false);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x05);
-//   delay(delaytime);
-//   lc.setChar(0,0,'d',false);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x1c);
-//   delay(delaytime);
-//   lc.setRow(0,0,B00010000);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x15);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x1D);
-//   delay(delaytime);
-//   lc.clearDisplay(0);
-//   delay(delaytime);
-// }
-
-// /*
-//   This method will scroll all the hexa-decimal
-//  numbers and letters on the display. You will need at least
-//  four 7-Segment digits. otherwise it won't really look that good.
-//  */
-// void scrollDigits() {
-//   for(int i=0;i<13;i++) {
-//     lc.setDigit(0,3,i,false);
-//     lc.setDigit(0,2,i+1,false);
-//     lc.setDigit(0,1,i+2,false);
-//     lc.setDigit(0,0,i+3,false);
-//     delay(delaytime);
-//   }
-//   lc.clearDisplay(0);
-//   delay(delaytime);
-// }
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
